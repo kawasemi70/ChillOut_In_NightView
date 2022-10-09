6675ddacf900d32d3a859e629b1775f25e6a8aef
kawasemi70 <108577146+kawasemi70@users.noreply.github.com>
2022-10-09 15:48:38 +0000

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,53 @@
 Rails.application.routes.draw do
- 
 
-  devise_for :admins
-  devise_for :customers
+   #会員様
+ scope module: 'public' do
+    devise_for :customers,skip: [:passwords], controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
+    root to: "homes#top"
+    get "about" => "homes#about"
+
+    #検索ボタンが押された時、Searchesコントローラーのsearchアクションが実行されるように定義。
+    #パス名は__「search_path」__
+    get 'search' => 'searches#search'
+    resources :spots, only: [:index]
+    resources :customers, only: [:index, :show, :edit, :update] do
+      get 'favorites' => 'customers#favorites'
+      patch 'release' => 'customers#release'
+      patch 'nonrelease' => 'customers#nonrelease'
+
+    end
+    resources :posts do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+ end
+
+
+
+ #ゲストログイン
+ devise_scope :customers do
+    post 'customers/guest_sign_in', to: 'public/sessions#guest_sign_in'
+ end
+
+
+  #管理者用
+
+   devise_for :admin, skip: [:registrations, :passwords], controllers: {
+      sessions: 'admin/sessions'
+    }
+
+  namespace :admin do
+    get "/" => "homes#top"
+
+    resources :customers, only: [:index, :show, :destroy]
+    resources :posts, only: [:index, :show, :destroy]
+
+  end
+
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_145332) do
+ActiveRecord::Schema.define(version: 2022_10_09_015409) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2022_10_08_145332) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "post_id", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,6 +72,32 @@ ActiveRecord::Schema.define(version: 2022_10_08_145332) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "title", null: false
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "spots", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_spots_on_post_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "spots", "posts"
 end
